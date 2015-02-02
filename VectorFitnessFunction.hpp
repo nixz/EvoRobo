4f5584b2c79c55bfe9140b4e01ee96d73503b36f
Nikhil Shetty <nikhil.j.shetty@gmail.com>
2015-02-01 20:50:28 -0700

    Initial commit

---
+++ VectorFitnessFunction.hpp
@@ -0,0 +1,33 @@
+/*
+ * VectorFitnessFunction.hpp
+ *
+ *  Created on: Jan 22, 2015
+ *      Author: Joost Huizinga
+ *
+ * This will be the fitness function for the vector individual.
+ */
+
+#ifndef VECTORFITNESSFUNCTION_HPP_
+#define VECTORFITNESSFUNCTION_HPP_
+
+#include "VectorIndividual.hpp"
+
+class VectorFitnessFunction
+{
+public:
+    /**
+     * Function that evaluates and individual and returns its fitness.
+     *
+     * Implement such that the fitness of the vector is the average of its elements.
+     *
+     * Use individual.setFitness(fitness) to assign the fitness to the individual once calculated.
+     */
+    void evaluate(VectorIndividual& individual){
+        /* TODO: YOUR CODE HERE */
+
+    }
+};
+
+
+
+#endif /* VECTORFITNESSFUNCTION_HPP_ */
---
+++ main.cpp
@@ -0,0 +1,24 @@
+/*
+ * Main.cpp
+ *
+ *  Created on: Jan 22, 2015
+ *      Author: Joost Huizinga
+ *
+ * This is the main file for homework 1.
+ */
+
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <math.h>
+
+/* Assignment 1 */
+#include "HillClimber.hpp"
+#include "VectorIndividual.hpp"
+#include "VectorFitnessFunction.hpp"
+#include "Random.hpp"
+
+int main(int argc, char* argv[]) {
+    /* TODO: YOUR CODE HERE */
+
+    return 0;
+}
