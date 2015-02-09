2f423df5cbae3f7f6649bc9e2d42333bd900857e
Nikhil Shetty <nikhil.j.shetty@gmail.com>
2015-02-09 01:20:50 -0700

    initialize the values and run for 50 steps and log

--- cpp/Ann/main.cpp
+++ cpp/Ann/main.cpp
@@ -22,5 +22,17 @@ int main(int argc, char* argv[]) {
     networkFile << network << "\n";
     networkFile.close();
 
+    std::ofstream activationFile("hw2_activation.csv");
+    // Initiate the network with random values
+    for (int i=0; i < network.getNeurons().size(); i++) {
+        network.getNeurons()[i].setValue( randDouble());
+    }
+    // Run the network for 50 time steps and log the activation
+    for (int i=0; i < 50; i++) {
+        network.step();
+        network.logActivation( activationFile );
+    }
+
+
     return 0;
 }
